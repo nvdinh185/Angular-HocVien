ed9490e661858ebe2520e6cafaf22c0a6a1be934
nvdinh185 <nvdinh0905671805@gmail.com>
2018-11-24 11:14:10 +0700

    push to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HocVienComponent } from './hoc-vien/hoc-vien.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HocVienComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
